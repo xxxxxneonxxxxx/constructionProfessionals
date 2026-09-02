186016ca4ee71a502ad642970685ee2f61e3aeef
xxxxxneonxxxxx <xneoshax@gmail.com>
2026-09-02 13:39:37 +0700

    Format phone and disable scroll reveal motion

--- Front/src/scripts/main.js
+++ Front/src/scripts/main.js
@@ -3,4 +3,3 @@ import "../components/before-after/before-after.js";
 import "../components/header/header.js";
 import "../components/price-summary/price-summary.js";
 import "../components/lead-form/lead-form.js";
-import "../components/motion/motion.js";
